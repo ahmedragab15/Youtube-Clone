940d7e54c7c60f5c047ec76bfa582da659aaf5da
ahmedragab15 <ragabahmed154@gmail.com>
2025-05-14 22:52:07 +0300

    last.2

--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -5,11 +5,12 @@ import { VideoCard, ChannelCard, Loader } from "./index";
 type Direction = "row" | "column";
 
 const Videos = ({ videos, direction }: { videos: IVideos[]; direction?: Direction }) => {
-  if (!videos.length) return <Loader />;
+
+  if (!videos?.length) return <Loader />;
 
   return (
     <Stack direction={direction || "row"} flexWrap={"wrap"} justifyContent={"start"} gap={2}>
-      {videos.map((item, idx) => (
+      {videos?.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
           {item.id.channelId && <ChannelCard channelDetail={item} marginTop="0" />}

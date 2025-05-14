456c86a7b045d1580a58af9a7244416e095e49c4
ahmedragab15 <ragabahmed154@gmail.com>
2025-05-14 22:49:41 +0300

    last

--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -1,29 +1,20 @@
-import { Stack, Box, Typography } from "@mui/material";
+import { Stack, Box } from "@mui/material";
 import type { IVideos } from "../interfaces";
-import { VideoCard, ChannelCard } from "./index";
+import { VideoCard, ChannelCard, Loader } from "./index";
 
 type Direction = "row" | "column";
 
 const Videos = ({ videos, direction }: { videos: IVideos[]; direction?: Direction }) => {
+  if (!videos.length) return <Loader />;
+
   return (
     <Stack direction={direction || "row"} flexWrap={"wrap"} justifyContent={"start"} gap={2}>
-      {videos?.length > 0 ? (
-        videos.map((item, idx) => (
-          <Box key={idx}>
-            {item.id.videoId && <VideoCard video={item} />}
-            {item.id.channelId && <ChannelCard channelDetail={item} marginTop="0" />}
-          </Box>
-        ))
-      ) : (
-        <Box sx={{ textAlign: "center", mt: 5 }}>
-          <Typography variant="h6" color="error">
-            API quota has been exceeded. Please try again later.
-          </Typography>
-          <Typography variant="body2" color="gray">
-            Please try again later or contact the site owner if this issue persists.
-          </Typography>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} marginTop="0" />}
         </Box>
-      )}
+      ))}
     </Stack>
   );
 };
